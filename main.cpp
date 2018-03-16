28e99390168f0755287aca10c4e12ba8c5cdd869
Xiaofeng Qu <xiaofeng.qu.83@gmail.com>
2018-03-16 11:01:20 -0400

    Add functions to compressed the image. Introduce the k value. Files are in ASCII format.

--- main.cpp
+++ main.cpp
@@ -5,25 +5,33 @@
 #include <sstream>
 #include "jama_svd.h"
 #include "tnt.h"
+#include "half.hpp"
 
 using namespace TNT;
 using namespace JAMA;
+using half_float::half;
+using namespace half_float::detail;
 using namespace std;
 
 void helpInfo();
 bool checkFilePath(string);
-Array2D<double> fileToMatrix(string); // Read the pgm file into a 2D Matrix
+Array2D< double > fileToMatrix(string, int&, int&, int&); // Read the pgm file into a 2D Matrix
 void pgma2b(const string&);
 void pgmb2a(const string&);
-void convertInitialPgmToASCII(string);
-void compressInitialPgm(string);
-void convertCompressedPgmToASCII(string);
+void compressPgm(string, int);
+void compressPgma(string, int);
+void SVD2Pgm(string);
+void SVDB2Pgm(string);
+Array2D< double > product(Array2D< double >, Array2D< double >);
+Array2D< double > transpose2D(Array2D<double>);
+double norm2(Array2D< double >, Array2D< double >);
+long fileSize(string);
 
 int main(int argc, char** argv)
 {
     // Check whether there are two arguments, if not exit and return to the command line
-    if(argc != 3){
-        cout << "There should be two arguments. Please correct your input." << endl;
+    if(argc != 3 && argc != 4){
+        cout << "The number of inputs is wrong. Please correct your input." << endl;
         helpInfo();
         return -1;
     }
@@ -35,60 +43,25 @@ int main(int argc, char** argv)
     }
 
     char option = argv[1][0];
- //   cout << option << endl;
     switch(option){
         case '1': pgma2b(filePath);
                 break;
         case '2': pgmb2a(filePath);
                 break;
-        case '3': break;
-        case '4': break;
+        case '3': {
+                istringstream ss(argv[3]);
+                int k;
+                ss >> k;
+                compressPgma(filePath, k);
+                break;}
+        case '4': SVD2Pgm(filePath);
+                break;
         default: cout << "Something wrong!" << endl;
                 break;
     }
 
-
-//    ifstream infile(filePath);
-//    string line;
-//    char firstChar;
-//    getline(infile, line);
-//    cout << line[0] << endl;
-    // Read the initial pgm file into a 2D matrix
-//    ifstream infile(filePath);
-//    string line;
-//    while(true){
-//        getline(inputFile, line);
-//        istringstream ss(line);
-//        char firstChar
-//        ss >> firstChar;
-//
-//    }
-//
-//    int M = 5, N = 5;
-//    Array2D< double > A(M,N, 0.0);    /* create MxN array; all zeros */
-//    for (int i=0; i < M; i++)
-//        for (int j=0; j < N; j++)
-//            A[i][j] = rand() % 100;              /* initalize array values */
-//    Array2D< double > B = A.copy();       /* create a new copy */
-//    Array2D< double > C(B);               /* create a new view of B */
-//                                         /* Both arrays (B & C) share data */
-//    for (int i=0; i < M; i++){
-//        for (int j=0; j < N; j++){
-//            cout << A[i][j] << "\t";
-//        }
-//        cout << endl;
-//    }
-//    cout<<endl;
-    Array2D< double > A = fileToMatrix(filePath);
-    cout << endl;
-    SVD< double > D(A);
-    D.getV(A);
-    for (int i=0; i < 4; i++){
-        for (int j=0; j < 5; j++){
-            cout << A[i][j] << "\t";
-        }
-        cout << endl;
-    }
+//    compressPgma("./bin/debug/baboon.pgm", 32);
+//    SVG2Pgm("./bin/debug/baboon.pgm.svd");
     return 0;
 }
 
@@ -113,10 +86,9 @@ bool checkFilePath(string filePath){
     return true;
 }
 
-Array2D<double> fileToMatrix(string filePath){
+Array2D< double > fileToMatrix(string filePath, int &row, int &col, int &grayScale){
     ifstream infile(filePath);
     string line;
-    int row, col, grayScale;
     getline(infile, line);
     getline(infile, line);
     infile >> row >> col >> grayScale;
@@ -125,12 +97,12 @@ Array2D<double> fileToMatrix(string filePath){
     for (int i=0; i < row; i++)
         for (int j=0; j < col; j++)
             infile >> A[i][j];
-    // Test the 2d Array
-    for (int i=0; i < row; i++){
-        for (int j=0; j < col; j++)
-            cout << A[i][j] << "\t";
-        cout << "\n";
-    }
+//    // Test the 2d Array
+//    for (int i=0; i < row; i++){
+//        for (int j=0; j < col; j++)
+//            cout << A[i][j] << "\t";
+//        cout << "\n";
+//    }
     return A;
 }
 
@@ -144,8 +116,10 @@ void pgma2b(const string& filename) {
 
       unsigned int height, width, buffer;
       unsigned char greyscale;
-      char magic_number[2];
-      pgm_ascii >> magic_number[0] >> magic_number[1] >> height >> width >> buffer;
+      string line;
+      getline(pgm_ascii, line);
+      getline(pgm_ascii, line);
+      pgm_ascii >>height >> width >> buffer;
       greyscale = buffer;
       vector<unsigned char> image(height * width, 0);
       vector<unsigned char>::iterator pixel = image.begin();
@@ -154,8 +128,6 @@ void pgma2b(const string& filename) {
             *pixel = (unsigned char)buffer;
             cout << (int)(*pixel) << ' ';
       }
-      magic_number[1] = '5';
-      pgm_bin.write(magic_number, 2);
       pgm_bin.write((reinterpret_cast<char*>(&height)), 1).
               write((reinterpret_cast<char*>(&height))+ 1, 1).
               write((reinterpret_cast<char*>(&width)), 1).
@@ -178,9 +150,7 @@ void pgmb2a(const string& filename) {
 
       unsigned int height = 0, width = 0;
       unsigned char greyscale;
-      char magic_number[2];
-      pgm_bin.read(magic_number, 2).
-              read(reinterpret_cast<char*>(&height), 1).
+      pgm_bin.read(reinterpret_cast<char*>(&height), 1).
               read(reinterpret_cast<char*>(&height) + 1, 1).
               read(reinterpret_cast<char*>(&width), 1).
               read(reinterpret_cast<char*>(&width) + 1, 1).
@@ -191,12 +161,233 @@ void pgmb2a(const string& filename) {
             cout << *pixel << ' ';
             pgm_bin.read(reinterpret_cast<char*>(&(*pixel)), 1);
       }
-      magic_number[1] = '2';
-      pgm_ascii << magic_number[0] << magic_number[1] << '\n' << height << ' ' << width << '\n'
+      pgm_ascii << height << ' ' << width << '\n'
                 << (unsigned int)greyscale << '\n';
       for (pixel = image.begin(); pixel != image.end(); ++pixel) {
-            pgm_ascii << ' ' << (unsigned int)(*pixel);
+            pgm_ascii << (unsigned int)(*pixel) << '\t';
       }
       pgm_ascii.close();
       pgm_bin.close();
 }
+
+void compressPgm(string filePath, int k){
+    int row, col, grayScale;
+    Array2D< double > pgmMatrix = fileToMatrix(filePath, row, col, grayScale);
+    Array2D< double > U(pgmMatrix), S(pgmMatrix), V(pgmMatrix);
+    SVD< double > SU(U), SS(S), SV(V);
+    SU.getU(U);
+    SS.getS(S);
+    SV.getV(V);
+    int rankS = SU.rank();
+    if(rankS < k){
+        cout << "k is greater than the rank, " << rankS << ". Please give a smaller k.";
+        return;
+    }
+    if(k == 0)
+        k = rankS;
+//    for(int i=0; i < U.dim1(); i++){
+//        for(int j=0; j < U.dim2(); j++)
+//            cout << U[i][j] << '\t';
+//        cout << '\n';
+//    }
+//    cout << '\n';
+//    for(int i=0; i < S.dim1(); i++){
+//        for(int j=0; j < S.dim2(); j++)
+//            cout << S[i][j] << '\t';
+//        cout << '\n';
+//    }
+//    cout << '\n';
+//    for(int i=0; i < V.dim1(); i++){
+//        for(int j=0; j < V.dim2(); j++)
+//            cout << V[i][j] << '\t';
+//        cout << '\n';
+//    }
+    // Write to the binary file
+    string new_name(filePath.begin(), filePath.end() - 4);
+    new_name += ".pgm.SVD";
+    ofstream pgm_asc;
+    pgm_asc.open(new_name.c_str(), ios_base::in);
+    pgm_asc << row << col << grayScale;
+    cout << row << " " << col << " " << grayScale << endl;
+    pgm_asc.close();
+//    ofstream pgm_bin;
+//    pgm_bin.open(new_name.c_str(), ios_base::out | ios_base::binary);
+//    unsigned int height, width, greyscale;
+//    pgm_bin.write((reinterpret_cast<char*>(&height)), 1).
+//            write((reinterpret_cast<char*>(&height)) + 1, 1).
+//            write((reinterpret_cast<char*>(&width)), 1).
+//            write((reinterpret_cast<char*>(&width)) + 1, 1).
+//            write(reinterpret_cast<char*>(&greyscale), 1).
+//            write(reinterpret_cast<char*>(&k), 1);
+//    for(int i=0; i < U.dim1(); i++){
+//        for(int j=0; j < k; j++)
+//              cout << U[i][j];
+//
+//    }
+    for(int i=0; i < k; i++){
+//        half hNum = half_cast<half>(S[k][k]);
+//        cout << hNum << '\t';
+        cout << S[i][i] << '\t';
+//        pgm_bin.write((reinterpret_cast<char*>(&hNum)), 1).
+//                write((reinterpret_cast<char*>(&hNum)) + 1, 1);
+    }
+//    for(int i=0; i < k; i++){
+//        for(int j=0; j < V.dim2(); j++)
+//            half hNum = half_cast<half>(V[i][j]);
+////            pgm_bin.write((reinterpret_cast<char*>(&hNum)), 1).
+////                    write((reinterpret_cast<char*>(&hNum)) + 1, 1);
+//    }
+}
+
+void compressPgma(string filePath, int k){
+    int row, col, grayScale;
+    Array2D< double > pgmMatrix = fileToMatrix(filePath, row, col, grayScale);
+    Array2D< double > U(pgmMatrix), S(pgmMatrix), V(pgmMatrix);
+    SVD< double > SU(U), SS(S), SV(V);
+    SU.getU(U);
+    SS.getS(S);
+    SV.getV(V);
+    Array2D< double > VT = transpose2D(V);
+    int rankS = SU.rank();
+    if(rankS < k){
+        cout << "k is greater than the rank, " << rankS << ". Please give a smaller k.";
+        return;
+    }
+    if(k == 0)
+        k = rankS;
+    string new_name(filePath.begin(), filePath.end() - 4);
+    new_name += ".pgm.SVD";
+    ofstream svd_asc;
+    svd_asc.open(new_name.c_str());
+    svd_asc << row << " " << col << " " << k << "\n" << grayScale << "\n";
+    for(int i = 0; i < U.dim1(); i++){
+        for(int j=0; j < k; j++)
+            svd_asc << U[i][j] << " ";
+        svd_asc << "\n";
+    }
+    for(int i = 0; i < k; i++){
+        svd_asc << S[i][i] << " ";
+    }
+    svd_asc << "\n";
+    for(int i = 0; i < k; i++){
+        for(int j=0; j < VT.dim2(); j++)
+            svd_asc << VT[i][j] << " ";
+        svd_asc << "\n";
+    }
+    svd_asc.close();
+}
+
+void SVD2Pgm(string filePath){
+    string new_name(filePath.begin(), filePath.end() - 8);
+    new_name += "_asc.pgm";
+    ifstream svd_asc;
+    ofstream pgm_asc;
+    svd_asc.open(filePath);
+    pgm_asc.open(new_name);
+    int row, col, k, grayScale;
+    svd_asc >> row >> col >> k >> grayScale;
+    cout << row << " " << col << " " << k << " " << grayScale << endl;
+    pgm_asc << "P2\n";
+    pgm_asc << row << " " << col << "\n";
+    pgm_asc << grayScale << "\n";
+    Array2D< double > U(row, k, 0.0);
+    Array2D< double > S(k, k, 0.0);
+    Array2D< double > VT(k, col, 0.0);
+    for(int i = 0; i < row; i++)
+        for(int j = 0; j < k; j++)
+            svd_asc >> U[i][j];
+    for(int i = 0; i < k; i++)
+        svd_asc >> S[i][i];
+    for(int i = 0; i < k; i++)
+        for(int j = 0; j < col; j++)
+            svd_asc >> VT[i][j];
+    // Display U,S,V
+//    for(int i = 0; i < row; i++){
+//        for(int j = 0; j < k; j++)
+//            cout << U[i][j] << "\t";
+//        cout << "\n";
+//    }
+//    cout << endl;
+//    for(int i = 0; i < k; i++){
+//         for(int j = 0; j < k; j++)
+//            cout << S[i][j] << "\t";
+//         cout << "\n";
+//    }
+//    cout << endl;
+//    for(int i = 0; i < k; i++){
+//        for(int j = 0; j < col; j++)
+//            cout << V[i][j] << "\t";
+//        cout << "\n";
+//    }
+    // Display USV
+    Array2D< double > US(product(U, S));
+    Array2D< double > USV(row, col, 0.0);
+    try{
+        USV = product(US, VT);
+    } catch (string e){
+        cout << e << endl;
+    }
+    for(int i = 0; i < USV.dim1(); i++){
+        for(int j = 0; j < USV.dim2(); j++)
+            pgm_asc << USV[i][j] << " ";
+        pgm_asc << "\n";
+    }
+//    for(int i = 0; i < USV.dim1(); i++){
+//        for(int j = 0; j < USV.dim2(); j++)
+//            cout << USV[i][j] << "\t";
+//        cout << "\n";
+//    }
+    svd_asc.close();
+    pgm_asc.close();
+}
+
+Array2D< double > product(Array2D< double > A, Array2D< double > B){
+    int rowA = A.dim1();
+    int colA = A.dim2();
+    int rowB = B.dim1();
+    int colB = B.dim2();
+//    cout << "rowA" << rowA << endl;
+//    cout << "colA" << colA << endl;
+//    cout << "rowB" << rowB << endl;
+//    cout << "colB" << colB << endl;
+    if(colA != rowB){
+        throw string("Cannot multiply these two matrices.");
+    }
+    Array2D< double > P(rowA, colB, 0.0);
+    for(int i = 0; i < rowA; i++){
+        for(int j = 0; j < colB; j++){
+            for(int k = 0; k < colA; k++){
+                P[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+    return P;
+}
+
+Array2D< double > transpose2D(Array2D< double > A){
+    Array2D< double > T(A.dim2(), A.dim1(), 0.0);
+    for(int i = 0; i < T.dim1(); i++)
+        for(int j = 0; j < T.dim2(); j++)
+            T[i][j] = A[j][i];
+    return T;
+}
+
+double norm2(Array2D< double > A, Array2D< double > B){
+    double n2 = 0.0;
+    if (A.dim1() != B.dim1() || A.dim2() != B.dim2()){
+        throw string("The two matrices do not have the same dimension.");
+    }
+    for(int i = 0; i < A.dim1(); i++)
+        for(int j = 0; j < A.dim2(); j++)
+            n2 += pow(abs(A[i][j] - B[i][j]), 2);
+    return sqrt(n2);
+}
+
+long fileSize(string filePath){
+    ifstream in(filePath, ios::binary|ios::ate);
+    long fs = in.tellg();
+    in.close();
+    return fs;
+}
+
+
